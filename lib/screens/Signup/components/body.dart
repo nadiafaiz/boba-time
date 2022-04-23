4d0bc5a6876d3b159102dca2f6ba0f915caab88d
nadiafaiz <faiznadia01@gmail.com>
2022-04-22 23:36:31 -0500

    Add scroll view to login/signup screens to prevent overflow

--- lib/screens/Signup/components/body.dart
+++ lib/screens/Signup/components/body.dart
@@ -8,54 +8,52 @@ import 'package:flutter/material.dart';
 import '../../../components/already_have_account_check.dart';
 
 class Body extends StatelessWidget {
-  const Body({
-    Key? key
-  }) : super(key: key);
+  const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const Text(
-            "SIGN UP",
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          const Image(
-            image: AssetImage(
-              'assets/images/signup.png',
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "SIGN UP",
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
-            fit: BoxFit.cover,
-          ),
-          RoundedInputField(
-            hintText: "Full Name",
-            onChanged: (value) {},
-          ),
-          RoundedInputField(
-            hintText: "Your email",
-            onChanged: (value) {},
-          ),
-          RoundedPasswordField(
+            const Image(
+              image: AssetImage(
+                'assets/images/signup.png',
+              ),
+              fit: BoxFit.cover,
+            ),
+            RoundedInputField(
+              hintText: "Full Name",
               onChanged: (value) {},
-          ),
-          RoundedButton(
+            ),
+            RoundedInputField(
+              hintText: "Your email",
+              onChanged: (value) {},
+            ),
+            RoundedPasswordField(
+              onChanged: (value) {},
+            ),
+            RoundedButton(
               text: "SIGN UP",
               press: () {},
-          ),
-          AlreadyHaveAccountCheck(
-            login: false,
-            press: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const LoginScreen()),
-              );
-            },
-          ),
-      ],
-    ),
+            ),
+            AlreadyHaveAccountCheck(
+              login: false,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginScreen()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
-
